00178c91af724dc3867b1e91f311d4993b3bac74
tzwm <wangjunzhe4444@gmail.com>
2013-05-26 11:27:19 +0800

    mody activity_main

--- src/com/d2/soundalarm/MainActivity.java
+++ src/com/d2/soundalarm/MainActivity.java
@@ -1,85 +1,98 @@
 package com.d2.soundalarm;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.*;
+
+import java.util.Vector;
 
 /**
  * Created by tzwm on 5/25/13.
  */
 
-class DrawView extends View{
-
-
-    public DrawView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    @Override
-    public void onDraw(Canvas canvas){
-        Paint p = new Paint();
-        canvas.drawCircle(60, 20, 10, p);
-        p.setAntiAlias(true);
-
-    }
-}
+//class DrawView extends View{
+//
+//
+//    public DrawView(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//    }
+//
+//    @Override
+//    public void onDraw(Canvas canvas){
+//        //wai 252,226,193
+//        //nei 223,216,208
+//        Paint wai = new Paint();
+//        wai.setColor(Color.rgb(252, 226, 193));
+//        canvas.drawCircle(400, 200, 200, wai);
+//        wai.setAntiAlias(true);
+//
+//        Paint nei = new Paint();
+//        nei.setColor(Color.rgb(223, 216, 208));
+//        canvas.drawCircle(400, 200, 150, nei);
+//        nei.setAntiAlias(true);
+//    }
+//}
 
 public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        topText = (TextView)findViewById(R.id.topView);
+        topText.setText("请勿怒摔手机");
+
         btnTime = (Button)findViewById(R.id.btnTime);
         btnRepeat = (Button)findViewById(R.id.btnRepeat);
-        btnRing = (Button)findViewById(R.id.btnRing);
-        btnType = (Button)findViewById(R.id.btnType);
+        btnDel = (Button)findViewById(R.id.btnDel);
+        btnMem = (Button)findViewById(R.id.btnMem);
 
-        topText = (TextView)findViewById(R.id.topView);
-
-        topText.setText("aha");
-
-        OnClickListener listTime = new Button.OnClickListener(){
+        OnClickListener list4Button = new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                topText.setText("Time");
+                if(view.getId() == R.id.btnTime)
+                    topText.setText("Time");
+                if(view.getId() == R.id.btnRepeat)
+                    topText.setText("Repeat");
+                if(view.getId() == R.id.btnDel)
+                    topText.setText("Ring");
+                if(view.getId() == R.id.btnMem)
+                    topText.setText("Type");
             }
         };
-        btnTime.setOnClickListener(listTime);
 
+        btnTime.setOnClickListener(list4Button);
+        btnRepeat.setOnClickListener(list4Button);
+        btnDel.setOnClickListener(list4Button);
+        btnMem.setOnClickListener(list4Button);
 
-//        img = (ImageView)findViewById(R.id.turntable);
-//        bitImg = ((BitmapDrawable)(img.getDrawable())).getBitmap();
-//        img.setOnClickListener(listTime);
-//        img.setOnTouchListener(new Button.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
+        img = (ImageView)findViewById(R.id.turntable);
+        bitImg = ((BitmapDrawable)(img.getDrawable())).getBitmap();
+        img.setOnClickListener(list4Button);
+        img.setOnTouchListener(new Button.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
 //                topText.setText(String.valueOf(String.format("%x", bitImg.getPixel((int) (motionEvent.getX()), (int) (motionEvent.getY())))));
-//                if(( bitImg.getPixel((int)(motionEvent.getX()), (int)(motionEvent.getY())) & 0xff000000 ) != 0){
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
+                if(( bitImg.getPixel((int)(motionEvent.getX()), (int)(motionEvent.getY())) ) == 0){
+                    return false;
+                }
+                return true;
+            }
+        });
+
+        Button btnTmp = (Button)findViewById(R.id.btn0);
+        btnTmp.setBackgroundResource(R.drawable.on);
 
         topText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
-
-
-    private Button btnTime, btnRepeat, btnRing, btnType;
+    private Vector<Button> btn;
+    private Button btnTime, btnRepeat, btnDel, btnMem;
     private TextView topText;
     private ImageView img;
     private Bitmap bitImg;
